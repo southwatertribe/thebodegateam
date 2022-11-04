f0393ad9664862420098800c8750c40d4d37ccf4
RobinRosculete <robin.rosculete.860@my.csun.edu>
2022-11-04 15:31:48 -0700

    Added a Create CFO Profile prototype page to test Data base

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
 import { Route, Routes } from "react-router-dom";
-
-import './App.css';
+import "./App.css";
 //Pages
 import Browser from "./pages/Browser";
 import Welcome from "./pages/Welcome";
 import About from "./pages/About";
 import Header from "./components/header/Header";
+import CreateCFOProfile from "./pages/CreateCFOProfile";
 
 function App() {
   // const [data, setData] = React.useState(null);
@@ -19,25 +18,25 @@ function App() {
   // }, []);
 
   return (
-    <>
-      <Header/>
+    <div className="Initial page">
+      <Header />
       <Routes>
-        <Route path="/" element={<Welcome/>} /> {}
-        <Route path="/browser" element={<Browser/>} />
-        <Route path="/about" element={<About/>}/>
+        <Route path="/" element={<Welcome />} /> {}
+        <Route path="/browser" element={<Browser />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/CreateCFOProfile" element={<CreateCFOProfile />} />
       </Routes>
-    </>
+    </div>
   );
 }
 
 export default App;
 
-
 // <div className="App">
 //       {/* <header className="App-header">
 //         <img src={logo} className="App-logo" alt="logo" />
 //         <p>
-//         {!data ? "loading..." : data} 
+//         {!data ? "loading..." : data}
 //         </p>
 //         <a
 //           className="App-link"
