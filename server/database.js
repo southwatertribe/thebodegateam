a5a21151712f9273ccfbbdd437eba91feb0ba25c
RobinRosculete <robin.rosculete.860@my.csun.edu>
2022-11-05 12:34:25 -0700

    Connected to AWS Mysql Database

---
+++ server/database.js
@@ -0,0 +1,19 @@
+const mysql = require("mysql2");
+require("dotenv").config();
+
+const connection = mysql.createConnection({
+  host: process.env.RDS_HOSTNAME,
+  user: process.env.RDS_USERNAME,
+  password: process.env.RDS_PASSWORD,
+  port: process.env.RDS_PORT,
+});
+
+connection.connect(function (err) {
+  if (err) {
+    console.error("Database connection  failed: " + err.stack);
+    return;
+  }
+  console.log("Connected to database.");
+});
+
+connection.end();
