9b5a0f1503b6c6f3f6b3b210919b7b0d9886ecc2
Gago-H <pgag32@gmail.com>
2022-11-08 00:21:28 -0800

    I added some text and messed around with HTML/CSS to clean it up and add substance to the page

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,9 +1,38 @@
 import React from 'react'
+import './About.css';
 
 function About() {
   return (
     //Make an about page
-    <div>About</div>
+    <div>
+    <head>
+        <title>About</title>
+    </head>
+    <body>
+      <header>
+        <div class="box-1">
+        <h2>Learn about us!</h2>
+        </div>
+      </header>  
+
+      <section>
+        <article>
+        <div class="box-2">
+        <p>
+            Bodega is a startup group of university students
+            who want to help out the CFO's of the world. What is
+            a CFO? CFO stands for "Cottage Food Operators" and 
+            these guys are the self-made chefs at home who want to
+            make a business preparing, cooking, and selling food
+            right outside their homes. But how would people be able
+            to find out about them, let alone trust them? That's what
+            Bodega's for.
+        </p>
+        </div>
+        </article>
+      </section>
+    </body>
+    </div>
   )
 }
 
