587bdd8427aa6dc498f6d4455a3a6994a1b0affd
Bayans90 <bayan.saab.577@my.csun.edu>
2022-11-17 11:02:14 -0800

    added a picture
    part1/4 of adding pictures

--- client/src/pages/Welcome/Welcome.js
+++ client/src/pages/Welcome/Welcome.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import Message from '../../components/message/message'
-import Img1 from '../Welcome/Pictures for Home/Image1.jpg';
+import Img1 from '../Welcome/Image1.jpg';
 
 export default function Welcome() {
   return (
     //MAIN WELCOME PAGE
     <div>
-      <Message/>
-      <img 
-      src= {Img1}
-      alt="Honey"
-      width="100" />
+      <img
+      src= {Img1} 
+      align= "right"
+      alt='honey'
+      width="300"
+      height="200"
+      />
 
     </div>
   )
