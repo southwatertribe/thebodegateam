9c8918465d0ab8762624e8f9aa8f18bdbfb22bcc
Danny Carter <imdannycarter@gmail.com>
2022-11-21 14:21:30 -0800

    Danny changes

--- server/app.js
+++ server/app.js
@@ -5,6 +5,8 @@ const path = require("path");
 const dbconnect = require("./database/database_connection.js");
 //routes
 const shopsRouter = require("./routes/shops.js");
+
+const connection = require("./database/database_connection.js");
 //const { default: dbconnect } = require('./database/database_conn.js');
 
 var isprod = process.env.NODE_ENV === "production";
@@ -17,15 +19,14 @@ const startServer = async () => {
   //Routes Middle Ware
   app.use("/shops", shopsRouter);
 
+
   console.log(`NODE_ENV=${process.env.NODE_ENV}`);
-  app.get("/api", (req, res) => {
-    res.json({ message: "Updating foh docker" });
-  });
 
   app.listen(process.env.PORT, () => {
     console.log(`Spun up on ${process.env.PORT}`);
     if (isprod == false) console.log("running in dev mode");
   });
+  dbconnect.dbconnect();
 };
 
 startServer();
--- server/database/database_connection.js
+++ server/database/database_connection.js
@@ -3,28 +3,25 @@ const mysql = require("mysql2");
 
 require("dotenv").config();
 
-const connection = mysql.createConnection({
-  host: process.env.RDS_HOSTNAME,
-  user: process.env.RDS_USERNAME,
-  password: process.env.RDS_PASSWORD,
-  port: process.env.RDS_PORT,
-  multipleStatements: true /* turn on multiple statements */,
-});
+exports.dbconnect = function () {
 
-// connection.connect(function (err) {
-//   if (err) {
-//     console.error("Database connection  failed: " + err.stack);
-//     return;
-//   }
-//   console.log("Connected to database.");
-// });
-connection.connect(function (err) {
-  try {
-    console.log("Connected to database")
-  } catch (error) {
-    console.error("Database connection  failed: " + err.stack);
-     return;
-  }
-})
+  const connection = mysql.createConnection({
+    host: process.env.RDS_HOSTNAME,
+    user: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    port: process.env.RDS_PORT,
+    multipleStatements: true /* turn on multiple statements */,
+  });
+  
+  
+  connection.connect(function (err) {
+    try {
+      console.log("Connected to database")
+    } catch (error) {
+      console.error("Database connection  failed: " + err.stack);
+       return;
+    }
+  })
+}
 
-module.exports = connection;
+
