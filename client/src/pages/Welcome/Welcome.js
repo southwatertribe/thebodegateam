2579227581280b6544364b834faace00241fb4b4
Bayans90 <bayan.saab.577@my.csun.edu>
2022-11-21 19:42:51 -0800

    Finished prototype for home page/welcome
    The welcome site prototype has been finished

--- client/src/pages/Welcome/Welcome.js
+++ client/src/pages/Welcome/Welcome.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Message from '../../components/message/message'
-import Style from '../Welcome/Pictures/image.module.css';
 import Img1 from '../Welcome/Pictures/Image1.jpg';
 import Img2 from '../Welcome/Pictures/Image2.jpg';
 import Img3 from '../Welcome/Pictures/Image 3.jpg';
@@ -10,16 +9,17 @@ import Img4 from '../Welcome/Pictures/Image 4.jpg';
 export default function Welcome() {
   return (
     //MAIN WELCOME PAGE
-    <div className="flex justify-right my-3">
+    <div className="stretch justify-space-around my-6">
+      
        <img src= {Img1} align = "right" alt='honey'
-        width="300" height="200"/>
-      <img src= {Img2} align="right" alt='honey'
-        width="300" height="200"/>
-      <img src= {Img3} align="right" alt='honey'
-        width="300" height="200"/>
-      <img src= {Img4} align="right" alt='honey'
-        width="300" height="200"/>  
-      <Message/>
+        width="250" height="200"/>
+      <img src= {Img2} align="right" alt='market'
+        width="250" height="200"/>
+      <img src= {Img3} align="right" alt='chef'
+        width="250" height="200"/>  
+      <img src= {Img4} align="right" alt='transaction'
+        width="250" height="200"/>  
+        <Message/>
     </div>
     
   )
