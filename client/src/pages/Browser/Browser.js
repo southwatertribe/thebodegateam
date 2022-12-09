811b18f2a8a7a6d274ba3f9045abd2d439cf6d09
RobinRosculete <robin.rosculete.860@my.csun.edu>
2022-12-09 14:17:46 -0800

    Fixed moudle bug

--- client/src/pages/Browser/Browser.js
+++ client/src/pages/Browser/Browser.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 //import { Document, Page } from "@react-pdf/renderer";
 import "../pages.css";
 import Axios from "axios";
